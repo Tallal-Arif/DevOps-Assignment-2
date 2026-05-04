2b6b766de8c1a958d8ede07226553e8a9e83e56a
Tallal-Arif <talalansari29@gmail.com>
2026-05-05 00:36:21 +0500

    Revert "Added Assginment 3"
    This reverts commit bcc9e6bc62903de6c6dc2b451c944515ecea45b6.

--- Assignment 3/Assignment 3/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-assignment-3-bce35194"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locking-bce35194"
-    encrypt        = true
-  }
-}
